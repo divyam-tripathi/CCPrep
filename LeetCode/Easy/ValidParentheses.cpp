5e8602c48c38fde878c875f198a47e1ff58ff9c6
divyam-tripathi <divyam.tri2611@gmail.com>
2021-05-08 12:48:30 +0530

    Added solution for ValidParentheses leetcode problem

---
+++ LeetCode/Easy/ValidParentheses.cpp
@@ -0,0 +1,58 @@
+/* 
+
+Question 20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order. */
+
+#include<iostream>
+#include<stack>
+#include<map>
+
+using namespace std;
+
+class Solution {
+    map<char, char> brackets_map = {
+        {'}', '{'},
+        {']', '['},
+        {')', '('}
+    }; 
+public:
+    bool isValid(string s) {
+        if(s.empty() || isEndBracket(s.at(0))) {
+            return false;
+        }
+        stack<char> s_stack;
+        for(char s_char : s) {
+            if(!isEndBracket(s_char) || s_stack.empty()) {
+                s_stack.push(s_char);
+            } else {
+                if(s_stack.top() == brackets_map[s_char]) {
+                        s_stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(s_stack.empty()) {
+            return true;
+        }
+        return false; 
+    }
+
+    bool isEndBracket(char c) {
+        return c == '}' || c == ']' || c == ')' ;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    cout << sol.isValid("(){}}{") << endl;
+    return 0;
+}
